36fad9af24a804968d66c59b1d61bc77a25c29bd
Medi, Erim <erim.medi@haw-hamburg.de>
2024-12-13 00:08:58 +0100

    Initial commit: Basic project setup with Spring Boot

---
+++ src/main/java/com/nexuschess/nexuschess/NexuschessApplication.java
@@ -0,0 +1,13 @@
+package com.nexuschess.nexuschess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuschessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuschessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexuschess/nexuschess/NexuschessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexuschess.nexuschess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuschessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
